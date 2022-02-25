40219fbe275af7da9379ba78fcb1af31c2f327a1
TakumiKimura <taku1219@DESKTOP-825ECAJ.localdomain>
2022-02-25 15:35:12 +0900

    [add] jqueryで移動データを保存 保存されたデータが存在する場合データベースにアクセスしてその値を更新

--- index.php
+++ index.php
@@ -35,6 +35,20 @@ if(!empty($_POST['inputName'])) {
         echo 'データベースにアクセス出来ません!'.$e->getMessage();
     }
 }
+
+/* 移動したようその座標をデータベースへ登録 */
+if(!empty($_POST['left'])){
+  try{
+    $sql  = 'UPDATE `sortable` SET `left_x` = :LEFT, `top_y` = :TOP WHERE `id` = :NUMBER';
+    $stmt = $dbh->prepare($sql);
+    $stmt->bindParam(':LEFT'  , $_POST['left'], PDO::PARAM_INT);
+    $stmt->bindParam(':TOP'   , $_POST['top'],  PDO::PARAM_INT);
+    $stmt->bindParam(':NUMBER', $_POST['id'],   PDO::PARAM_INT);
+    $stmt->execute();
+  } catch (PDOException $e) {
+    echo $e->getMessage();
+  }
+}
 ?>
 
 <!DOCTYPE html>
@@ -75,5 +89,44 @@ if(!empty($_POST['inputName'])) {
             </div>
         </div>
     </div>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1/jquery-ui.min.js"></script>
+    <script>
+    $(function(){
+      $('.drag').draggable({       /* class="drag"が指定されている要素をdraggableに */
+        containment:'#drag-area',  /* ドラッグできる範囲 */
+        cursor:'move',             /* ドラッグ時のカーソル形状 */
+        opacity:0.6,               /* ドラッグ中の透明度 */
+        scroll:true,               /* ウィンドウ内をスクロールしたい */
+        zIndex:10,                 /* ドラッグ中の重ね順を一番上に */
+        /* ==========STOP処理====================================== */
+        stop:function(event, ui){               /* ドラッグ終了時に起動 */
+          var myNum  = $(this).data('num');     /* data-num="値" の値を取得 */
+          var myLeft = (ui.offset.left - $('#drag-area').offset().left);    /* leftの座標　= 要素の座標 - #drag-areaの座標 */
+          var myTop  = (ui.offset.top  - $('#drag-area').offset().top);     /* topの座標 　= 要素の座標 - #drag-areaの座標 */
+          /* ==========AJAX通信================= */
+        $.ajax({
+            type:'POST',      /* typeパラメーター：POSTかGETか */
+            url :'http://localhost:80/sortable/',  /* urlパラメーター：飛ばす先のファイル名（今回は自分に戻ってくる） */
+            data: {           /* dataパラメーター：データをphpに渡す data:{ foo:'引数', bar:'引数' }　でPHP側は $_POST['foo'] で'引数'の値を受け取る */
+              id  :myNum,     /* key:valueの関係、つまりidのというkeyとそれに入れる値valueはmyNumとなる */
+              left:myLeft,
+              top :myTop
+            }
+          }).done(function(){   /* ajaxの通信に成功した場合の処理 */
+             console.log('成功');
+          }).fail(function(XMLHttpRequest, textStatus, errorThrown){  /* ajaxの通信に失敗した場合エラー表示 */
+             console.log(XMLHttpRequest.status);
+             console.log(textStatus);
+             console.log(errorThrown);
+          });
+          /* ==========/AJAX通信================= */
+                console.log("左: " + myLeft);     /* x座標がとれている */
+                console.log("上: " + myTop);      /* y座標がとれている */
+            }
+            /* ==========/STOP処理====================================== */
+      });
+    });
+    </script>
 </body>
 </html>
