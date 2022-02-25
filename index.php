a122f8d2ffe5774d9655429c730044b6dfebdceb
TakumiKimura <taku1219@DESKTOP-825ECAJ.localdomain>
2022-02-25 14:20:39 +0900

    [add] データベースに接続　データを配列で表示

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
+<?php
+    error_reporting(-1);
+    
+    /* データベース設定 */
+    define('DB_DNS', 'mysql:host=localhost; dbname=cri_sortable; charset=utf8');
+    define('DB_USER', 'root');
+    // define('DB_PASSWORD', 'root');
+    
+    /* データベースへ接続 */
+    try {
+    //   $dbh = new PDO(DB_DNS, DB_USER, DB_PASSWORD);
+      $dbh = new PDO(DB_DNS, DB_USER,);
+      $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    } catch (PDOException $e){
+        echo $e->getMessage();
+        exit;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -10,7 +30,20 @@
 <body>
     <div id="wrapper">
         <div id="drag-area">
-            drag area
+            <?php 
+            $name = '木村拓光';
+            $message = 'こんにちは!';
+
+            echo $name.'さん'.$message;
+
+            $sql = 'SELECT * FROM sortable';
+            $stmt = $dbh->query($sql);
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            print_r($result) 
+            ?>
+
+            
         </div>
     </div>
 </body>
